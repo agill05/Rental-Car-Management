737aed6d9baa4398d23f89fb2acb6b22417724e7
agill05 <agilabddul30@gmail.com>
2025-12-16 01:52:00 +0800

    terbaru

--- database/migrations/2025_12_13_070442_create_peminjaman_table.php
+++ database/migrations/2025_12_13_070442_create_peminjaman_table.php
@@ -14,15 +14,11 @@ return new class extends Migration
             $table->foreignId('mobil_id')->constrained('mobils');
             $table->foreignId('pelanggan_id')->constrained('pelanggans');
             $table->foreignId('supir_id')->nullable()->constrained('supirs');
-
             $table->date('tanggal_pinjam');
             $table->date('tanggal_kembali_rencana');
-            
             $table->integer('lama_sewa');
             $table->decimal('harga_total', 12, 2);
-            
             $table->enum('status', ['dipinjam', 'menunggu_persetujuan', 'dikembalikan'])->default('dipinjam');
-            
             $table->timestamps();
         });
     }
